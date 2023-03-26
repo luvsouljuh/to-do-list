149bd8a5aec6837b633cf70b8e50b8d785658a14
Mateusz Mikosik <mateusz.mikosik@wp.pl>
2023-03-26 01:59:29 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+{
+    const tasks = [
+        {
+            content: "wykonać pracę",
+            done: false,
+            },
+        {
+            content: "ostylować strone",
+            done: true,
+        },
+    ];
+
+    const render =() => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+            document.querySelector(".js-tasks").innerHTML = htmlString;
+        }
+    }
+
+    const init = () => {
+
+        render ();
+
+    };
+
+    init();
+}
